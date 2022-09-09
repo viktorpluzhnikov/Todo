5d7799474fc2b70e0dda82d19dedf12ebd6ed18b
majahed228 <pluzhnikov93@inbox.ru>
2022-09-09 19:56:49 +0300

    created model Todo & Todolist

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        name_category = ('Category')
+        name_category_plural = ('Categories')       #множественное имя для Категорий
+
+    def __str__(self):
+        return self.name        # __str__ применяется для отображения объекта в интерфейсе
+
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField(blank=True)
+    created_at = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))      # Дата создания
+    date_end = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))        # До какой даты нужно сделать
+    category = models.ForeignKey(Category, default='general', on_delete=models.CASCADE())
+
+    class Meta:     # вспомогательный класс мета для сортировки наших дел
+        ordering = ['-created']     # сортировка дел по времени их создания
+
+    def __str__(self):
+        return self.title
